afb8c77bc862dd33cceea701667b498d27d89b2b
CristopherRAC <cristopherarmas23@gmail.com>
2019-10-20 16:43:46 -0500

    commit

--- src/app/examples/signup/signup.component.ts
+++ src/app/examples/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { SignUpService } from './services/signup.services';
 import { Router } from '@angular/router';
 import { UsuarioSistema } from './domain/UsuarioSistema';
@@ -9,6 +9,10 @@ import { UsuarioSistema } from './domain/UsuarioSistema';
     styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+    @Input()
+    public alerts: Array<IAlert> = [];
+    private backup: Array<IAlert>;
+
     test : Date = new Date();
 
     usuarioSistema: UsuarioSistema;
@@ -18,6 +22,7 @@ export class SignupComponent implements OnInit {
 
     clave: string;
     usuarioa: string;
+    mostrarAlerta: boolean = false;
 
 
 
@@ -27,28 +32,53 @@ export class SignupComponent implements OnInit {
 
         this.usuarioSistema = new UsuarioSistema();
 
+
+
+        this.alerts.push({
+            id: 1,
+            type: 'danger',
+            message: 'Usuario o clave incorrectos',
+            icon: 'nc-bell-55'
+        });
+        this.backup = this.alerts.map((alert: IAlert) => Object.assign({}, alert));
+
+
     }
 
 
     login(){
-
         this.service.getPersonaId(this.usuarioSistema.usuario, this.usuarioSistema.contrasen)
         .subscribe(data=>{
-
             if(data == null || data == undefined){
                 console.log('ERROR');
+                this.mostrarAlerta = true;
             }
-        
             if(data !== null && data !== undefined){
+                this.mostrarAlerta = false;
                 this.usuarioSistema=data;
-                console.log('EXITO');
-                console.log(this.usuarioSistema);
+                window.location.href = 'http://localhost:4201/#/';
             }
+        });
+    }
 
-          
-        })
+
+    public closeAlert(alert: IAlert) {
+        // const index: number = this.alerts.indexOf(alert);
+        // this.alerts.splice(index, 1);
+
+        this.mostrarAlerta = false;
 
     }
 
 
+
+
+}
+
+
+export interface IAlert {
+    id: number;
+    type: string;
+    message: string;
+    icon?: string;
 }
